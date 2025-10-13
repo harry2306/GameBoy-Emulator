185d5f4cb081dcdec542fdda5d16929e738623a3
harry2306 <halvarado2306@gmail.com>
2025-10-13 18:37:17 -0500

    Update Memory.cpp

--- Program/Memory.cpp
+++ Program/Memory.cpp
@@ -1,5 +1,16 @@
 #include "Memory.h";
 
 Memory::Memory(){
-  mem.resize(,0x00); //64 KB
+  mem.resize(0x10000,0x00); //64 KB
+}
+
+uint8_t Memory::Read(uint16_t addr) const{
+  if (addr < mem.size())
+      return mem[addr];
+  return 0xFF;
+}
+
+void Memory::Write(uint16_t addr, uint8_t value) const{
+  if (addr < mem.size())
+      mem[addr] = value;
 }
